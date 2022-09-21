45e35884156c2aea364b909a6d76901b4dc039be
SebastianBustamante <chaverras@uninorte.edu.co>
2022-09-21 15:01:37 -0500

    Gestión notas update

--- app.py
+++ app.py
@@ -1,10 +1,60 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+
 app=Flask(__name__)
 
 @app.route('/')
-def index(): 
-	return '<h1>Pagina Con Flask</h1>'
+def index():
+    return render_template('login1.html')
 
-if __name__=='__main__':
-    app.run(debug=True)
-    "app.run(debug=True, port=XXXX)"
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/login1')
+def login1():
+    return render_template('login1.html')
+
+@app.route('/clave')
+def clave():
+    return render_template('clave.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+
+@app.route('/usuarios')
+def menu_user():
+    return render_template('usuarios.html')
+
+@app.route('/materias')
+def menu_materias():
+    return render_template('materias.html')
+
+
+@app.route('/cursos')
+def menu_cursos():
+    return render_template('cursos.html')
+
+
+@app.route('/matriculas')
+def menu_matriculas():
+    return render_template('matriculas.html')
+
+
+
+@app.route('/actividades')
+def menu_actividades():
+    return render_template('actividades.html')
+
+
+
+@app.route('/calificaciones')
+def menu_calificaciones():
+    return render_template('calificaciones.html')
+
+if  __name__=='__main__':
+     app.run(debug=True)  
+
--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\sebas\MisionTIC\Programación\CICLO 3_HTML_CCS_JAVASCRIPT\ProyectoGestion_Notas\env
+set VIRTUAL_ENV=E:\sebas\MisionTIC\Programación\CICLO 3_HTML_CCS_JAVASCRIPT\ProyectoGestion_Notas1\env
 
 if not defined PROMPT set PROMPT=$P$G
 
