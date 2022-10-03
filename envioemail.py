86b1a8f8878efe4c9b77be5e2e8558f3c5777e16
PachangasFlow <ing.francisco.ossa@gmail.com>
2022-10-03 18:10:47 -0500

    ultimos cambios completos de mensajeria

--- envioemail.py
+++ envioemail.py
@@ -15,6 +15,6 @@ def enviar_email(email_destino,codigo):
     #email.set_content(mensaje)
     smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
     smtp.starttls()
-    smtp.login(remitente, "xxxxxxx")
+    smtp.login(remitente, "xxxxxxxx")
     smtp.sendmail(remitente, destinatario, email.as_string())
     smtp.quit()
