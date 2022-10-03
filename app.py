16b20fe33680684eee319a068c706c6a9ab5b61b
PachangasFlow <ing.francisco.ossa@gmail.com>
2022-10-03 15:11:36 -0500

    ultimos cambios clase 30-09-2022

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+import re
 from tkinter.messagebox import RETRY
-from flask import Flask, render_template, url_for, request, redirect, flash
+from flask import Flask, render_template, url_for, request, redirect, flash, session
 import controlador
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -33,23 +34,47 @@ def val_user():
     username = datos['username']
     passwd = datos['password']
     if username == '' or passwd == '':
-        flash('datos incompetos')
+        flash('Datos Incompletos')
     else:
         resultado = controlador.validar_usuarios(username)
         if resultado == False:
             flash('error al ingresar')
             return redirect(url_for('login'))
         else:
-
+            print('Resultado: ' + str(resultado[0]['verificado']))
             if (resultado[0]['verificado'] == 1):
 
                 if check_password_hash(resultado[0]['passwd'], passwd):
-                    return redirect(url_for('menu'))
+                    session['username'] = username
+                    session['nombre'] = resultado[0]['nombre'] + \
+                        " "+resultado[0]['apellido']
+                    listadouser = controlador.listar_usuarios()
+                    print(listadouser)
+                    return render_template('mensajeria.html', datauser=listadouser)
                 else:
                     flash('Contraseña Invalida')
                     return redirect(url_for('login'))
             else:
                 return redirect(url_for('verificar'))
+
+
+@app.route('/enviarmensajes', methods=['POST'])
+def enviar_mesanjes():
+    datos = request.form
+    remitente = session['username']
+    asunto = datos['asunto']
+    destinatario = datos['destinatario']
+    cuerpo = datos['cuerpo']
+    resultado = controlador.insertar_mensajes(
+        remitente, destinatario, asunto, cuerpo)
+    if resultado:
+        flash('Mensaje Enviado Correctamente')
+
+    else:
+        flash('Error en el Envio')
+
+    listadouser = controlador.listar_usuarios()
+    return render_template('mensajeria.html', datauser=listadouser)
 
 
 @app.route('/addregistro', methods=['POST'])
@@ -136,6 +161,9 @@ def registro():
 def verificar():
     return render_template('verificacion.html')
 
+@app.route('/mensajeria')
+def mensajeria():
+    return render_template('mensajeria.html')
 
 @app.route('/mensajes')
 def mensajes():
