028b9133c1b9de429c62baaf596a617b81229cb1
SebastianBustamante <chaverras@uninorte.edu.co>
2022-10-11 08:53:41 -0500

    Update

--- envioemail.py
+++ envioemail.py
@@ -3,31 +3,31 @@ import smtplib
 
 
 def enviar_email(email_destino, codigo):
-    remitente = "franciscoossa@uninorte.edu.co"
+    remitente = "chaverras@uninorte.edu.co"
     destinatario = email_destino
     mensaje = "Correo de activación"
 
     email = EmailMessage()
     email['From'] = remitente
     email['To'] = destinatario
-    email['Subject'] = 'Coonfirmación de Correo'
+    email['Subject'] = 'Confirmación de Correo'
     email.set_content("Bienvenido, Para Confirmar su cuenta Ingrese el Siguiente Codigo. \n codigo de verificación: " +
-                      codigo+" \n Recuerde ingresar este codigo para poder valisar si cuenta")
+                      codigo+" \n Recuerde ingresar este codigo para poder validar su cuenta")
     # email.set_content(mensaje)
     smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
     smtp.starttls()
-    smtp.login(remitente, "xxxxxxx")
+    smtp.login(remitente, "jus3buch4")
     smtp.sendmail(remitente, destinatario, email.as_string())
     smtp.quit()
 
 
 def recuperar_email(email_destino):
-    remitente = "franciscoossa@uninorte.edu.co"
+    remitente = "chaverras@uninorte.edu.co"
     destinatario = email_destino
     mensaje="<hr>"
     mensaje = "<h2>Recuperación de Cuenta</h2>"
     mensaje = mensaje + "<a href='http://localhost:5000/restablecer/" + email_destino + \
-        "'>Ingrese Aquí para restablecer su Contraseña</a>"
+        "'>Ingrese Aquí para reestablecer su Contraseña</a>"
     mensaje=mensaje+ "<hr>"
     email = EmailMessage()
     email['From'] = remitente
@@ -36,6 +36,6 @@ def recuperar_email(email_destino):
     email.set_content(mensaje, subtype="html")
     smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
     smtp.starttls()
-    smtp.login(remitente, "xxxxxxx")
+    smtp.login(remitente, "jus3buch4")
     smtp.sendmail(remitente, destinatario, email.as_string())
     smtp.quit()
