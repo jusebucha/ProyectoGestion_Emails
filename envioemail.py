0c670e944bfe3b9b826aaf64dc20377c8e16c355
SebastianBustamante <chaverras@uninorte.edu.co>
2022-10-11 20:07:50 -0500

    update

--- envioemail.py
+++ envioemail.py
@@ -10,9 +10,9 @@ def enviar_email(email_destino, codigo):
     email = EmailMessage()
     email['From'] = remitente
     email['To'] = destinatario
-    email['Subject'] = 'Confirmación de Correo'
-    email.set_content("Bienvenido, para confirmar su cuenta ingrese el siguiente código. \n código de verificación: " +
-    codigo+" \n Recuerde ingresar este código para poder validar su cuenta")
+    email['Subject'] = 'Coonfirmación de Correo'
+    email.set_content("Bienvenido, Para Confirmar su cuenta Ingrese el Siguiente Codigo. \n codigo de verificación: " +
+                      codigo+" \n Recuerde ingresar este codigo para poder valisar si cuenta")
     # email.set_content(mensaje)
     smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
     smtp.starttls()
@@ -25,14 +25,14 @@ def recuperar_email(email_destino):
     remitente = "chaverras@uninorte.edu.co"
     destinatario = email_destino
     mensaje="<hr>"
-    mensaje = "<h2>Recuperación de cuenta</h2>"
+    mensaje = "<h2>Recuperación de Cuenta</h2>"
     mensaje = mensaje + "<a href='http://localhost:5000/restablecer/" + email_destino + \
-    "'>Ingrese aquí para restablecer su contraseña</a>"
+        "'>Ingrese Aquí para restablecer su Contraseña</a>"
     mensaje=mensaje+ "<hr>"
     email = EmailMessage()
     email['From'] = remitente
     email['To'] = destinatario
-    email['Subject'] = 'recuperar contraseña'
+    email['Subject'] = 'recuperar Contraseña'
     email.set_content(mensaje, subtype="html")
     smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
     smtp.starttls()
